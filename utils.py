99eb91ad66ac8333555bba1356c7d25f42b76a45
prajyott17 <prajyot.tamgadge25m@iimranchi.ac.in>
2026-05-28 10:32:05 +0530

    updated global styling

--- utils.py
+++ utils.py
@@ -1,12 +1,28 @@
 import streamlit as st
 import base64
+
 @st.cache_data(ttl=3600)
 def get_rate():
     return {
         "USD": 93,
         "EUR": 109
     }
+
 def load_css():
+
+    with open("assets/chicago-pneumatic.jpg", "rb") as image_file:
+        encoded = base64.b64encode(image_file.read()).decode()
+
     with open("style.css", "r", encoding="utf-8") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-        
+        css = f.read()
+
+    css = css.replace(
+        "CP_BACKGROUND_IMAGE",
+        f"data:image/png;base64,{encoded}"
+    )
+
+    st.markdown(
+        f"<style>{css}</style>",
+        unsafe_allow_html=True
+    )
+
