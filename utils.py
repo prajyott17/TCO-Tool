995c85c7570b038ce42e2435c88a93164b7f20bc
prajyott17 <prajyot.tamgadge25m@iimranchi.ac.in>
2026-05-28 10:22:54 +0530

    updated nav

--- utils.py
+++ utils.py
@@ -1,28 +1,12 @@
 import streamlit as st
 import base64
-
 @st.cache_data(ttl=3600)
 def get_rate():
     return {
         "USD": 93,
         "EUR": 109
     }
-
 def load_css():
-
-    with open("assets/cp_bg.png", "rb") as image_file:
-        encoded = base64.b64encode(image_file.read()).decode()
-
     with open("style.css", "r", encoding="utf-8") as f:
-        css = f.read()
-
-    css = css.replace(
-        "CP_BACKGROUND_IMAGE",
-        f"data:image/png;base64,{encoded}"
-    )
-
-    st.markdown(
-        f"<style>{css}</style>",
-        unsafe_allow_html=True
-    )
-
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        
