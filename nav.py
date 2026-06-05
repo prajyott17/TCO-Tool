879db3abbe45ea41ccacc06dcd29b08d44854d67
prajyott17 <prajyot.tamgadge25m@iimranchi.ac.in>
2026-06-05 11:15:28 +0530

    updated nav

--- nav.py
+++ nav.py
@@ -9,7 +9,7 @@ def top_nav(current_page):
     header_col1, header_col2 = st.columns([2, 6])
 
     with header_col1:
-        st.image("assets/logo.jpg", width=130)
+        st.image("assets/hero_bg.avif", width=130)
 
     rates = get_rate()
     usd = rates["USD"]
